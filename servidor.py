c18cb040b479136a944da76bb70b3d3c2b0d5352
Camila Parra <mc.parrad@uniandes.edu.co>
2021-10-04 19:56:50 -0500

    Finalizado

--- servidor.py
+++ servidor.py
@@ -18,38 +18,38 @@ sock.listen(25)
 # Separador
 SEPARATOR = "SEPARATOR"
 
-#Archivos a enviar
-file1 = "Archivos/PRUEBA.txt"
-file2 = "Archivos/archivo1.txt"
+# Archivos a enviar
+file1 = "Archivos/temp_100MB_file.txt"
+file2 = "Archivos/temp_250MB_file.txt"
 
-#Nombre archivos a enviar
-filename1 = "PRUEBA.txt"
-filename2 = "archivo1.txt"
+# Nombre archivos a enviar
+filename1 = "temp_100MB_file.txt"
+filename2 = "temp_250MB_file.txt"
 
-#Tamaño de los archivos
+# Tamaño de los archivos
 filesize1 = os.path.getsize(file1)
 filesize2 = os.path.getsize(file2)
 
-#Array vacio de conecciones
+# Array vacio de conecciones
 conexiones = []
 
-#Array vacio con tiempos de entrega del archivo
+# Array vacio con tiempos de entrega del archivo
 tiempos = []
 
 #Array vacio con variable que indica si la transmisión fue exitosa o no
 exitos = []
 
-#Nombre log
+# Nombre log
 LOG_FILENAME = datetime.now().strftime('./Logs/%Y_%m_%d_%H_%M_%S.log')
 
-#Variable para cerrar servidor
+# Variable para cerrar servidor
 fin = False
 
-#Tamaño del buffer
+# Tamaño del buffer
 BUFFER_SIZE = 1024
 
 
-#Función envío de archivos
+# Función envío de archivos
 def archivo(num_archivo, c):
     nombreArchivo = ''
     tamArchivo = 0
@@ -64,6 +64,7 @@ def archivo(num_archivo, c):
         arch = file2
     start_time = datetime.now()
     c.send(f"{nombreArchivo}{SEPARATOR}{tamArchivo}".encode())
+    time.sleep(1)
     with open(arch, "rb") as f:
         while True:
             bytes_read = f.read(BUFFER_SIZE)
@@ -75,9 +76,11 @@ def archivo(num_archivo, c):
             c.send(bytes_read)
     message = b'Finaliza transmision'
     c.send(message)
+    time.sleep(1)
     md5(c, arch)
 
-#Función de creación y envío de hash
+
+# Función de creación y envío de hash
 def md5(connection, fname):
     md5 = hashlib.md5()
     with open(fname, 'rb') as f:
@@ -88,13 +91,14 @@ def md5(connection, fname):
             md5.update(data)
     connection.send(md5.hexdigest().encode('ISO-8859-1'))
     data = connection.recv(BUFFER_SIZE)
-    mensaje2 = data.decode('utf-8')
+    mensaje2 = data.decode('ISO-8859-1')
     exito = 0
     if ('Los valores son iguales' in mensaje2):
         exito = 1
     elif ('Los valores son diferentes' in mensaje2):
         exito = 0
     exitos.append(exito)
+
 
 #Función para crear el log
 def log(filenameF, filesize, exitos, tiempos):
@@ -113,40 +117,41 @@ def log(filenameF, filesize, exitos, tiempos):
         i += 1
     return filename
 
+
 if __name__ == "__main__":
- while True:
-    threads = []
-    print('waiting for a connection')
-    num_archivo = int(input('¿Qué archivo desea enviar? (1: 100MB, 2: 250MB)'))
-    num_clientes = int(input('¿A cuantos clientes desea enviar el archivo?'))
-    nomArchivo = ''
-    tamArchivo = 0
-    if (num_archivo == 1):
-        nomArchivo = filename1
-        tamArchivo = filesize1
-    elif (num_archivo == 2):
-        nomArchivo = filename2
-        tamArchivo = filesize2
-    try:
-        while True:
-            connection, client_address = sock.accept()
-            data = connection.recv(BUFFER_SIZE)
-            print('received {!r}'.format(data))
-            mensaje = data.decode('utf-8')
-            if mensaje == ('Listo para recibir'):
-                conexiones.append(connection)
-            if len(conexiones) >= num_clientes:
-                for c in conexiones:
-                    x = threading.Thread(target=archivo, args=(num_archivo, c, ))
-                    x.start()
-                    time.sleep(1)
-                    threads.append(x)
-                for x in threads:
-                    x.join()
-                fin = True
+    while True:
+        threads = []
+        print('waiting for a connection')
+        num_archivo = int(input('¿Qué archivo desea enviar? (1: 100MB, 2: 250MB)'))
+        num_clientes = int(input('¿A cuantos clientes desea enviar el archivo?'))
+        nomArchivo = ''
+        tamArchivo = 0
+        if (num_archivo == 1):
+            nomArchivo = filename1
+            tamArchivo = filesize1
+        elif (num_archivo == 2):
+            nomArchivo = filename2
+            tamArchivo = filesize2
+        try:
+            while True:
+                connection, client_address = sock.accept()
+                data = connection.recv(BUFFER_SIZE)
+                print('received {!r}'.format(data))
+                mensaje = data.decode('ISO-8859-1')
+                if mensaje == ('Listo para recibir'):
+                    conexiones.append(connection)
+                if len(conexiones) >= num_clientes:
+                    for c in conexiones:
+                        x = threading.Thread(target=archivo, args=(num_archivo, c,))
+                        x.start()
+                        time.sleep(1)
+                        threads.append(x)
+                    for x in threads:
+                        x.join()
+                    fin = True
+                    break
+        finally:
+            filename = log(nomArchivo, tamArchivo, exitos, tiempos)
+            connection.close()
+            if fin:
                 break
-    finally:
-        filename = log(nomArchivo, tamArchivo, exitos, tiempos)
-        connection.close()
-        if fin:
-            break
